8b06f4917ec2ad7e0adff2538cdc50950546a026
mau698 <mau6987@outlook.es>
2026-09-26 08:47:40 -0400

    feat(metrics): crea modulo, controlador y servicio de metricas

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,9 +4,10 @@ import { ScheduleModule } from './schedule/schedule.module.js';
 import { AvailabilityModule } from './availability/availability.module.js';
 import { AppointmentsModule } from './appointments/appointments.module.js';
 import { DatabaseModule } from './database/database.module.js';
+import { MetricsModule } from './metrics/metrics.module.js';
 
 @Module({
-  imports: [ScheduleModule, AvailabilityModule, AppointmentsModule, DatabaseModule],
+  imports: [ScheduleModule, AvailabilityModule, AppointmentsModule, DatabaseModule, MetricsModule],
   controllers: [AppController],
 })
 export class AppModule {}
